f22bcd13b2408af96600347246797eaf2edc4f80
qp133 <quangphung133@gmail.com>
2022-06-01 16:37:29 +0700

    update hw lesson 1

---
+++ Lesson-1/src/HwLesson1.java
@@ -0,0 +1,20 @@
+public class HwLesson1 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
+
+        //So sánh giữa i++ và ++i
+//        ++i     tăng i lên rồi mới lấy i sử dụng
+//        i++     lấy i sử dụng trước rồi tăng i lên
+
+//        trong vòng lặp --i và ++i sẽ thực hiện nhanh hơn i-- và i++
+//        --i và ++i thì -- được thực hiện trước
+//        i++ thực hiện nhanh hơn i = i +1 hoặc  i+=1     vì    i++ là 1 lệnh
+    }
+}
